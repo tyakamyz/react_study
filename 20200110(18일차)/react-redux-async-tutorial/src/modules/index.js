89798c9d9bc8946b12d7c7f50bdc4f3382644d69
tyakamyz <tyakamyz@naver.com>
2020-01-13 18:57:36 +0900

    오류 수정

--- 20200110(18일차)/react-redux-async-tutorial/src/modules/index.js
+++ 20200110(18일차)/react-redux-async-tutorial/src/modules/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
 import counter from './counter';
 
-export default combineReducers(
+export default combineReducers({
     // 앞으로 추가될 무듈들이 이 위치에서 리덕스에 추가됨.
     counter
+    }
 );
